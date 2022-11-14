1a59d2cab092c25a702db8c26352bf976a6adf84
Liliana Gómez <83715511+ligomezm@users.noreply.github.com>
2022-11-15 03:50:25 +1100

    minor changes on script

--- Proyecto 2nd Jam/Assets/Scripts/EnemyDecreasingPlayerLife.cs
+++ Proyecto 2nd Jam/Assets/Scripts/EnemyDecreasingPlayerLife.cs
@@ -19,8 +19,7 @@ public class EnemyDecreasingPlayerLife : MonoBehaviour
 
         if (dist <= howClose)
         {
-            //player.GetComponent<PlayerHealth>().TakeDamage(0.06f);
-            player.GetComponent<PlayerHealth>().TakeDamage(1f);
+           player.GetComponent<PlayerHealth>().TakeDamage(0.06f);
         }
 
     }
