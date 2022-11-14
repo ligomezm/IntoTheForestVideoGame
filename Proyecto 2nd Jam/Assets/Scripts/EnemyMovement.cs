241bf29ed4d913dbdafc1568b9a8d5f19cdc4c58
kmparxn <andreskmpa@gmail.com>
2022-11-14 04:42:36 -0500

    enemy movement fixed

--- Proyecto 2nd Jam/Assets/Scripts/EnemyMovement.cs
+++ Proyecto 2nd Jam/Assets/Scripts/EnemyMovement.cs
@@ -1,25 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
-{
-    GameObject target;
-    float speed;
+{   
+    //GameObject target;
+    //float speed;
+    private NavMeshAgent agent = null;
+    [SerializeField] private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 3f;
-        target = GameObject.Find("Player");
+        //speed = 3f;
+        //target = GameObject.Find("Player");
+
+        GetReferences();
     }
 
-    
+
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        /*transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
 
         Vector3 direction = Vector3.RotateTowards(Vector3.forward, target.transform.position - transform.position, 2f, 0f);
-        transform.rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.LookRotation(direction);*/
+
+        MoveToTarget();        
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void GetReferences()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void RotateToTarget()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
     }
 }
--- Proyecto 2nd Jam/Assets/Scripts/HouseIncreasingHealth.cs
+++ Proyecto 2nd Jam/Assets/Scripts/HouseIncreasingHealth.cs
@@ -6,7 +6,7 @@ public class HouseIncreasingHealth : MonoBehaviour
 {
     private Transform player;
     private float playerHealth;
-    
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
